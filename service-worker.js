16b6d848be513f054a8f642684ab1183a4301964
suivo2001 <suivo2001@users.noreply.github.com>
2022-05-04 04:40:52 +0000

    deploy: d1487616c3778ab081bd6e035a0c731c857006c7

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.697b0bac052401e456a2c2df88852b91.js"
+  "./precache-manifest.a4d8a5094b84701ced8b7ef3cff38f0e.js"
 );
 
 self.addEventListener('message', (event) => {
